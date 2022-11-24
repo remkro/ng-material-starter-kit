84a2df0bf014160d72bda832860b973f97f3e054
Remigiusz Królak <krolak.remek@gmail.com>
2022-11-24 15:43:43 +0100

    implemented product details feature (according to exercise 3.3)

--- src/app/components/product-details/product-details.component-module.ts
+++ src/app/components/product-details/product-details.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 import { ProductDetailsComponent } from './product-details.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   declarations: [ProductDetailsComponent],
   providers: [],
   exports: [ProductDetailsComponent]
